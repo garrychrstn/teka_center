c6d4db00dd4be7a45f2191ae82f79d4f76d88fc1
garrychrstn <70214107+garrychrstn@users.noreply.github.com>
2023-01-06 22:53:07 +0700

    Add files via upload

---
+++ teka_center/static/index/index.js
@@ -0,0 +1,8 @@
+function showEve() {
+    document.getElementById("activities").style.display = "none";
+    document.getElementById("event").style.display = "block";
+}
+function showAct() {
+    document.getElementById("event").style.display = "none";
+    document.getElementById("activities").style.display = "block";
+}
